b92af5ffc2bf1d27aefbca45944d5b8e216eed22
msut <marr994@o2.pl>
2016-10-30 18:20:19 +0100

    add basic controller

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -1,5 +1,21 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
+public class Controller {
+    @FXML
+    private TextField fileName;
+    @FXML
+    private TextArea fileContent;
+    @FXML
+    private Button readBtn;
+    @FXML
+    private Button writeBtn;
+    @FXML
+    private Label statusLabel;
+    
 }
