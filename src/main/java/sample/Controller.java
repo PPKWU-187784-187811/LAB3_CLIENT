86a0ac00166356437b0030e130e2ec0a8bf5f60f
msut <marr994@o2.pl>
2016-11-08 13:02:23 +0100

    add read file view

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -14,7 +14,7 @@ import static utils.Const.FAILURE;
 import static utils.Const.SUCCESS;
 
 public class Controller {
-    ILibrary iLibrary = new SimpleLibrary();
+    private ILibrary iLibrary = new SimpleLibrary();
     @FXML
     private TextField fileName;
     @FXML
@@ -29,22 +29,48 @@ public class Controller {
     @FXML
     public void readClicked() {
         System.out.println("READ CLICKED");
+        disableButtons();
+        Task task = new Task<Void>() {
+            @Override
+            public Void call() throws Exception {
+                String text = iLibrary.readFile(fileName.getText(), (result) -> {
+                    Platform.runLater(() -> {
+                        if (resultIsSuccess(result)) {
+                            statusLabel.setText("Read from finished!");
+                        } else if (resultIsFaiulure(result)) {
+                            statusLabel.setText("Read from failed!");
+                        } else {
+                            statusLabel.setText(result);
+                        }
+                    });
+                });
+                fileContent.setText(text);
+                return null;
+            }
+        };
+        Thread th = new Thread(task);
+        th.setDaemon(true);
+        th.start();
+        enableButtons();
+    }
+
+    private boolean resultIsSuccess(String result) {
+        return result.equals(SUCCESS);
     }
 
     @FXML
     public void writeClicked() {
         System.out.println("WRITE CLICKED");
-        writeBtn.setDisable(true);
-        readBtn.setDisable(true);
+        disableButtons();
 
         Task task = new Task<Void>() {
             @Override
             public Void call() throws Exception {
                 iLibrary.writeFile(fileName.getText(), fileContent.getText(), (result) -> {
                     Platform.runLater(() -> {
-                        if (result.equals(SUCCESS)) {
+                        if (resultIsSuccess(result)) {
                             statusLabel.setText("Write to file finished!");
-                        } else if (result.equals(FAILURE)) {
+                        } else if (resultIsFaiulure(result)) {
                             statusLabel.setText("Write to file failed!");
                         } else {
                             statusLabel.setText(result);
@@ -57,8 +83,21 @@ public class Controller {
         Thread th = new Thread(task);
         th.setDaemon(true);
         th.start();
+        enableButtons();
+
+    }
+
+    private boolean resultIsFaiulure(String result) {
+        return result.equals(FAILURE);
+    }
+
+    private void enableButtons() {
         readBtn.setDisable(false);
         writeBtn.setDisable(false);
+    }
 
+    private void disableButtons() {
+        writeBtn.setDisable(true);
+        readBtn.setDisable(true);
     }
 }
